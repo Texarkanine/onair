4eb1aea74a66945d397a662da872db90a909a6ee
Texarkanine <texarkanine@protonmail.com>
2025-02-07 20:07:42 -0600

    try to add "register" endpoint to register signs for push notifications

--- server/server.py
+++ server/server.py
@@ -1,10 +1,15 @@
 import os
 import json
 import subprocess
+import sqlite
+import urllib
 
 from flask import Flask, jsonify, request
 
 STATE_FILE = "onair-state.dat"
+API_BASE = "/onair/api"
+API_VERSION = "v1"
+API_URL = f"{API_BASE}/{API_VERSION}"
 
 app = Flask(__name__)
 
@@ -22,7 +27,7 @@ def state_change(old, new):
         state_file.write(json.dumps(new))
     return new
 
-@app.route('/onair/api/v1/state', methods=['GET'])
+@app.route(f"{API_URL}/state", methods=['GET'])
 def get_state():
     if os.path.isfile(STATE_FILE):
         print(f"State file {STATE_FILE} exists...")
@@ -33,13 +38,22 @@ def get_state():
     else:
         return jsonify(False)
 
-@app.route('/onair/api/v1/state', methods=['PUT'])
+@app.route(f"{API_URL}/state", methods=['PUT'])
 def set_state():
     old_state = get_state()
     new_state = json.loads(request.data.decode('utf-8'))
     
     return jsonify(state_change(old_state, new_state))
 
+@app.route(f"{API_URL}/register", methods=['POST'])
+def register_sign():
+    # get desired callback point (should parse to a url)
+    client_text = json.loads(request.data.decode('utf-8'))
+    client_url = urllib.parse.urlparse(client_text)
+    print("would register url: " + client_url)
+
+
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
