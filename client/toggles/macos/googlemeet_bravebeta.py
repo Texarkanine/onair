67cc65fd6b347e45e7fda26e0046778e8f02396c
Texarkanine <198288999+Texarkanine@users.noreply.github.com>
2025-07-14 17:22:19 +0000

    feat(client/toggles/macos): Add support for "Brave Browser Beta" (#8)

---
+++ client/toggles/macos/googlemeet_bravebeta.py
@@ -0,0 +1,4 @@
+from toggles.macos.lib import googlemeet_chromium
+
+def run_and_call(callback, poll_interval_s=5, start_threshold=5, cpu_threshold=15):
+    googlemeet_chromium.run_and_call(callback, "Brave Browser Beta", poll_interval_s, start_threshold, cpu_threshold)
